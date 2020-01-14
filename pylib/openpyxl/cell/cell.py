07f3c629ff519063ba8d31617b62195b72a04bc2
Andrew Geiger <andrew.geiger@corsosystems.com>
2020-01-14 15:12:28 -0800

    Right, `next` actually has a slightly more advanced functionality.   So consolidated and updated.

--- pylib/openpyxl/cell/cell.py
+++ pylib/openpyxl/cell/cell.py
@@ -46,8 +46,7 @@ except AttributeError:
 try:
     next
 except NameError:
-    def next(it):
-        return it.next()
+    from openpyxl.compat.next import next
 else:
     locals()['next'] = next
 
